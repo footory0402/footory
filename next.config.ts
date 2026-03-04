11f672b8f5a79875f84a00f1dc1036ae396d95c4
footory0402 <footory0402@users.noreply.github.com>
2026-03-04 09:52:16 +0900

    vercel 값 수정

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "vzsfsssmrcucyggyvpgu.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
     ],
   },
   headers: async () => [
